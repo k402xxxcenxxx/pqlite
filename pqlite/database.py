72661f8e23da31b4e7b91c5431d66349e0bec926
Ken Hu <ken@KenROGG17>
2024-02-12 15:13:40 +0800

    Add: WIP tcp server template

---
+++ pqlite/database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+class Database:
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+
+    def execute_query(self, query):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute(query)
+            self.conn.commit()
+            return cursor.fetchall()
+        except sqlite3.Error as e:
+            print(f"Database error: {e}")
+        finally:
+            cursor.close()
+
+    def close(self):
+        self.conn.close()
