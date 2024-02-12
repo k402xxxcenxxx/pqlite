da812fbc7d9eba2c9564318effafa6328d9620d9
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2024-02-12 15:45:24 +0800

    Add: re-add missing files

---
+++ pqlite/__main__.py
@@ -0,0 +1,8 @@
+"""Entry point for pqlite."""
+
+from __future__ import annotations
+
+from pqlite.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ pqlite/base.py
@@ -0,0 +1,19 @@
+"""
+pqlite base module.
+
+This is the principal module of the pqlite project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+from __future__ import annotations
+
+# example constant variable
+NAME = "pqlite"
