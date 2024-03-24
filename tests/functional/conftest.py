b907968d83a0623a464ddd376418d5a34270a050
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2024-03-24 09:23:19 +0000

    Add: functional test for checking the consistency after various operations

---
+++ tests/functional/conftest.py
@@ -0,0 +1,100 @@
+import random
+import pytest
+
+from itertools import permutations
+import threading
+
+from pqlite.server import DistributedDatabaseServer
+from pqlite.client import DistributedDatabaseClient
+
+
+class TestOperation:
+    movie_list = [
+        "Titanic"
+        "The Shawshank Redemption",
+        "The Godfather",
+        "Inception",
+        "Pulp Fiction",
+        "The Dark Knight",
+        "Forrest Gump",
+        "Fight Club",
+        "The Matrix",
+        "Star Wars: Episode IV - A New Hope",
+        "Jurassic Park",
+        "The Lord of the Rings: The Fellowship of the Ring",
+        "Avatar",
+        "The Avengers",
+        "Harry Potter and the Sorcerer's Stone",
+        "Toy Story",
+        "The Lion King",
+        "Gladiator",
+        "Forrest Gump",
+        "The Silence of the Lambs",
+    ]
+
+    def __init__(self):
+        self.transaction = self.gen_transaction()
+
+    def gen_transaction(self):
+        pass
+
+class CreateTableOperation(TestOperation):
+
+    def gen_transaction(self):
+        return "CREATE TABLE movie(id INTEGER PRIMARY KEY, title TEXT UNIQUE, year INTEGER, score REAL)"
+
+class DeleteOperation(TestOperation):
+
+    def gen_transaction(self):
+        movie = random.choice(self.movie_list)
+        return f"DELETE FROM movie WHERE title = '{movie}'"
+
+class InsertOperation(TestOperation):
+
+    def gen_transaction(self):
+        movie = random.choice(self.movie_list)
+        year = random.choice(range(1900, 2024))
+        score = round(random.uniform(0, 10), 1)
+
+        return f"INSERT INTO movie (title, year, score) VALUES ('{movie}', {year}, {score}) ON CONFLICT (title) DO UPDATE SET score = {score}"
+
+@pytest.fixture(scope="module")
+def testing_pairs():
+
+    def testing_pair(id):
+        server_instance = DistributedDatabaseServer(
+            "localhost", 0, f"test_{id}.db"
+        )
+        server_thread = threading.Thread(
+            target=server_instance.start, daemon=True
+        )
+        server_thread.start()
+
+        client_instance = DistributedDatabaseClient(
+            server_instance.host, server_instance.port
+        )
+
+        return {"server": server_instance, "client": client_instance}
+
+    # Create instances
+    pairs = [testing_pair(i) for i in range(3)]
+
+    # Group the instances to one cluster
+    for p in permutations(range(3), 2):
+        pairs[p[0]]["server"].add_node(
+            pairs[p[1]]["server"].host, pairs[p[1]]["server"].port
+        )
+
+    yield pairs
+
+    # Clean up
+    for p in pairs:
+        p["server"].close()
+
+@pytest.fixture(scope="module")
+def testing_cases(request):
+    n_cases = request.param
+
+    cases = [CreateTableOperation()]
+    cases.extend([random.choices([InsertOperation(), DeleteOperation()], weights=[0.8, 0.2])[0] for _ in range(n_cases - 1)])
+    return cases
---
+++ tests/functional/test_consistency.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+from random import choice
+
+import pytest
+
+def test_server_execution_consistency(testing_pairs):
+    # randomly pick one to create table
+    pair = choice(testing_pairs)
+    response = pair["client"].send_message(
+        "sync:db_ops:CREATE TABLE movie(title, year, score)"
+    )
+    assert (
+        response == "Success: -1"
+    ), "Client should receive a create success response"
+
+    # make sure every one have this table
+    for testing_pair in testing_pairs:
+        response = testing_pair["client"].send_message(
+            "db_ops:SELECT name FROM sqlite_master"
+        )
+        assert (
+            response == "Success: [('movie',)]"
+        ), "Client should receive a created database"
+
+@pytest.mark.parametrize("testing_cases", [1000], indirect=True)
+def test_server_execution_consistency2(testing_pairs, testing_cases):
+    # randomly pick one to do the operation
+    for case in testing_cases:
+        pair = choice(testing_pairs)
+        pair["client"].send_message(
+            f"sync:db_ops:{case.transaction}"
+        )
+    
+    # every node should have the same output
+    final_results = []
+    for testing_pair in testing_pairs:
+        final_results.append(testing_pair["client"].send_message(
+            "db_ops:SELECT title, year, score FROM movie"
+        ))
+
+    assert all(r == final_results[0] for r in final_results), "All result should be the same"
